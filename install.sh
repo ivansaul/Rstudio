ea9d09fd7a775bcce2bc4159c189a373babcc17d
ivansaul <saul_2503@hotmail.com>
2022-06-12 16:28:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Install R and RStudio Server (Don't forget to update version to latest version)
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/' 
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 sudo apt install r-base -y
 
-sudo apt update -y 
-sudo apt install gdebi-core 
+sudo apt update -y
+sudo apt install gdebi-core
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.02.3-492-amd64.deb
 sudo gdebi -n rstudio-server-2022.02.3-492-amd64.deb -y
 
